d90cda2934774bae28d53d476446da83b49362b1
Levi S. Morris <siromivel@gmail.com>
2014-08-08 15:12:08 -0400

    ready for Heroku deployment with test db

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -7,5 +7,13 @@ class PartsController < ApplicationController
   def show
     @part = Part.find(params[:id])
   end
+  
+  def change
+    create_table :parts do |p|
+	  p.string :name
+	  p.string :component
+	  p.string :manufacturer
+	  p.integer :weight
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Part.create(name: "Hardrock" manufacturer: "Specialized" component: "frame" weight: 695)
+Part.create(name: "Rockhopper" manufacturer: "Specialized" component: "frame" weight: 657)
+Part.create(name: "Epic" manufacturer: "Specialized" component: "frame" weight: 701)
+Part.create(name: "Carbon Nomad" manufacturer: "Santa Cruz" component: "frame" weight: 655)
+Part.create(name: "Float 100" manufacturer: "Fox" component: "fork" weight: 437)
+Part.create(name: "Float 120" manufacturer: "Fox" component: "fork" weight: 465)
+Part.create(name: "Revelation" manufacturer: "Rock Shox" component: "fork" weight: 695)
+Part.create(name: "Spinners" manufacturer: "Spinnin Rims" component: "wheelset" weight: 1280)
+Part.create(name: "Arch" manufacturer: "Stan's" component: "wheelset" weight: 1123)
+Part.create(name: "Float" manufacturer: "Stan's" component: "wheelset" weight: 1046)
