5163c6f1b67e2b4e91bd13f2748e86d06d9226e1
Levi S. Morris <siromivel@gmail.com>
2014-08-11 10:34:39 -0400

    Finished migrating project over to postgreSQL.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140808144216) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "parts", force: true do |t|
     t.string   "component"
     t.string   "manufacturer"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@ Part.create(name: "Carbon Nomad", manufacturer: "Santa Cruz", component: "frame"
 Part.create(name: "Float 100", manufacturer: "Fox", component: "fork", weight: 437)
 Part.create(name: "Float 120", manufacturer: "Fox", component: "fork", weight: 465)
 Part.create(name: "Revelation", manufacturer: "Rock Shox", component: "fork", weight: 695)
-Part.create(name: "Spinners", manufacturer: "Spinnin Rims", component: "wheelset" weight: 1280)
+Part.create(name: "Spinners", manufacturer: "Spinnin Rims", component: "wheelset", weight: 1280)
 Part.create(name: "Arch", manufacturer: "Stan's", component: "wheelset", weight: 1123)
 Part.create(name: "Float", manufacturer: "Stan's", component: "wheelset", weight: 1046)
